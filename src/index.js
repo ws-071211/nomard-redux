36f94fb1ca114aabc2bd8d4a6e7fc06809fb4d2d
ws-071211 <swhsw1211@gmail.com>
2024-01-16 21:04:36 +0900

    1-0 counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const num = document.querySelector('span');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let count = 0;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+num.innerText = count;
+
+const updateText = () => {
+  num.innerText = count;
+}
+
+const plusEvent = () => {
+  count = count + 1;
+  updateText();
+}
+
+const minusEvent = () => {
+  count = count - 1;
+  updateText();
+}
+
+plus.addEventListener('click',plusEvent);
+minus.addEventListener('click',minusEvent);
