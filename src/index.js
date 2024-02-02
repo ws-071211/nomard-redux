ce8c798d55f6badccf0b6bef52276844f1482c5e
민우석 <s23044@gsm.hs.kr>
2024-02-02 23:59:21 +0900

    2-3 delete todo-2

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const reducer = (state = [], action) => {
     case addTodo:
       return [{ text: action.text, id: Date.now() }, ...state];
     case deleteTodo:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -42,7 +42,7 @@ const dispatchAddToDo = text => {
 };
 
 const dispatchDeleteToDo = e => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
