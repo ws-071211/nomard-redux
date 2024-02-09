1231e2f570294a3fccbd93ac8093d7e1171d3c8f
민우석 <s23044@gsm.hs.kr>
2024-02-09 23:59:57 +0900

    3-4 delete todo

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,17 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
+import { actionCreators } from "../store";
+import ToDo from "../components/ToDo";
 
-function Home({ toDos }) {
+
+function Home({ toDos, addToDo  }) {
   const [text, setText] = useState("");
   function onChange(e) {
     setText(e.target.value);
   }
   function onSubmit(e) {
     e.preventDefault();
+    addToDo(text);
     setText("");
   }
   return (
@@ -17,7 +21,11 @@ function Home({ toDos }) {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul>{JSON.stringify(toDos)}</ul>
+      <ul>
+        {toDos.map(toDo => (
+          <ToDo {...toDo} key={toDo.id} />
+        ))}
+      </ul>
     </>
   );
 }
@@ -26,4 +34,10 @@ function mapStateToProps(state) {
   return { toDos: state };
 }
 
-export default connect(mapStateToProps)(Home);
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: text => dispatch(actionCreators.addToDo(text))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/store.js
+++ src/store.js
@@ -13,7 +13,7 @@ const addToDo = text => {
 const deleteToDo = id => {
   return {
     type: DELETE,
-    id
+    id: parseInt(id)
   };
 };
 
